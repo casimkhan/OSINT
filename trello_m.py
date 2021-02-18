8c72c54c1c1f5680484751dd5728ebb1b032a9a2
covertshell <casimkhan@gmail.com>
2021-02-18 10:22:34 +0000

    Update trello_m.py

--- trello_m.py
+++ trello_m.py
@@ -22,33 +22,26 @@ def urlpar(url_in,apikey,apitoken):
                 print('Private board')
 
         url_main = "https://api.trello.com/1/boards/" + boardid + "/members"
-
         query = {
                 'key': apikey,
                 'token': apitoken
         }
-
         response = requests.get(url=url_main, params=query)
-
         data = response.json()
-
-
         user_list = []
-
         countu = ((len(data)) - 1 )
-
         for i in range(countu,0,-1):
                 user_name = (data[i]['username'])
                 user_list.append(user_name)
         return user_list
 
+## Function will take username and return URL list of boards
 def memboard(memname,apikey,apitoken):
         url_member = "https://api.trello.com/1/members/" + memname + "/boards"
         query = {
                 'key': apikey,
                 'token': apitoken
         }
-
         response1 = requests.get(url=url_member, params=query)
         meminf = response1.json()
         url_list = []
@@ -61,24 +54,23 @@ def memboard(memname,apikey,apitoken):
 if __name__ == "__main__":
 
 
-        parser = argparse.ArgumentParser(description="This Program will find all the possible boards by Iterating each member from URL")
+        parser = argparse.ArgumentParser(description="This Program will find all possible members of trello boards and Iterating each member to find further board URL")
         parser.add_argument("--url", type=str, help="Trello board url")
         args = parser.parse_args()
 
         url_in = args.url
-        apikey = 'YOUR KEY'
+ ## Your Trello API Key and API Token
+        apikey = 'YOUR API KEY'
         apitoken = 'Your token'
         mem_list = []
         memurl_list = []
         uname = urlpar(url_in,apikey,apitoken)
         for y in range(len(uname)):
                 print('User Name: %s' % uname[y])
-                json_body = memboard(uname[y],apikey,apitoken)
-                for x in range(len(json_body)):
-                        if json_body[x] not in memurl_list:
-                                print('New URL : %s' % json_body[x])
-                                memurl_list.append(json_body[x])
-                        else:
-                                print('Duplicate URL')
-
+                url_list_m = memboard(uname[y],apikey,apitoken)
+                for x in range(len(url_list_m)):
+                        if url_list_m[x] not in memurl_list:
+                                print('New URL : %s' % url_list_m[x])
+                                memurl_list.append(url_list_m[x])
+ 
         print('Total Public board URLs found: %d' % len(set(memurl_list)))
